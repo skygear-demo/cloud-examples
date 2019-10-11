1e2500ca07093fe68e720f038687030d09b005b6
kiootic <kiootic@oursky.com>
2019-10-11 16:13:23 +0800

    Configure Skygear SDK

--- reactjs-express/frontend/src/App.js
+++ reactjs-express/frontend/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import skygear from '@skygear/web';
 import AuthForm from './components/AuthForm';
 import './App.css';
 
+async function configureSkygear() {
+  try {
+    await skygear.configure({ endpoint: process.env.REACT_APP_API_ENDPOINT, apiKey: process.env.REACT_APP_API_KEY });
+    return true;
+  } catch (err) {
+    alert(err);
+    return false;
+  }
+}
+
 function App() {
+  const [configured, setConfigured] = useState(false);
+  useEffect(() => {
+    setConfigured(configureSkygear());
+  }, []);
+
   return (
     <div className="App">
-      <AuthForm />
+      {configured ?
+        <AuthForm /> :
+        null
+      }
     </div>
   );
 }
